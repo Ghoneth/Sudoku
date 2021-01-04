97c3586e2c3e5cf05bd223a1a52ddcdf5cd6dcef
Ghoneth <74926427+Ghoneth@users.noreply.github.com>
2021-01-04 14:53:33 +0100

    Update Premieralgo.java

--- Premieralgo.java
+++ Premieralgo.java
@@ -2,13 +2,17 @@ package premalgo;
 
 public class PremAlgo {
     int[][] grille;
-    int i=0;
+    int iteration=0;
+    int solution;
+    long tSolution;
     
-    public PremAlgo (int[][] sudokuPartVide) {
-        grille=sudokuPartVide;
+    public PremAlgo (int[][] sudokuPartiellementVide) { //Constructeur contenant le sudoku et son nomre de solution
+        grille=sudokuPartiellementVide;
+        solution=0;
+        tSolution=0;
     }
     
-    public void afficherSudoku() {
+    public void afficherSudoku() { //Methode permettant d'afficher un sudoku
         for (int ligne = 0; ligne < 9; ligne++) {
             System.out.print("{");
             for (int colonne = 0; colonne < 9; colonne++) {
@@ -17,64 +21,89 @@ public class PremAlgo {
             }
             System.out.println("}");
         }
-    }
+    }    
 
-    public boolean Resolver () {
+    public int resolver () { //Méthode récursive résolvant un sudoku en testant chaque case une par une
         for (int ligne = 0; ligne < 9; ligne++) {
             for (int colonne = 0; colonne < 9; colonne++) {
                 if (this.grille[ligne][colonne]==0) {
                     for (int valeur = 1; valeur < 10; valeur++) {
                         this.grille[ligne][colonne]=valeur;
-                        i++;
-                        System.out.println();
-                        System.out.println("Iteration n° : " + i);
-                        this.afficherSudoku();
-                        if (EstValide(ligne,colonne)) if (this.Resolver()) return true;
+                        if (solution==0) {
+                            iteration++;
+                            System.out.println();
+                            System.out.println("Iteration n° : " + iteration + " de la solution n° 1"); //Affichage des nombres trouvés au fur et à mesure pour la premiere solution
+                            this.afficherSudoku();
+                        }
+                        if (estValide()) this.resolver(); //Recursivité, test de la prochaine case
+                        if (ligne==9 && colonne==9) return 0;
                         this.grille[ligne][colonne]=0;
                     }
-                    return false;
+                    return 0;
                 }
             }
         }
-        return true;
+        solution++; //Incrémentation du nombre de solution
+        if (solution==1) {
+            System.out.println();
+            System.out.println("La solution la plus rapidement trouvée est :");
+            this.afficherSudoku();
+            System.out.println();
+            this.tSolution=new java.util.Date().getTime();
+        }
+        if (solution>1) {
+            System.out.println("Il existe une " + this.solution+ "e solution");
+            //this.afficherSudoku();  //Affichage des autres solutions si voulu
+            //System.out.println();
+        }
+        return 0;
     }
-            
-    public boolean EstValide (int ligne, int colonne) {
-        int valeur = this.grille[ligne][colonne];
-        for (int index=0; index<9; index++) {
-            if (this.grille[ligne][index]==valeur) if (index!=colonne) return false;
-            if (this.grille[index][colonne]==valeur) if (index!=ligne) return false;
-        }
-        int CLigne = ligne/3;
-        int CColonne = colonne/3;
-        for (int index=0+CLigne*3; index<3+CLigne*3; index++) {
-            for (int jndex=0+CColonne*3; jndex<3+CColonne*3; jndex++) {
-                if (this.grille[index][jndex]==valeur) if (index!=ligne && jndex!=colonne) return false;
+
+    public boolean estValide () { //Méthode testant si un algorithme rempli ou partiellement rempli est correct
+         for (int ligne = 0; ligne < 9; ligne++) {
+            for (int colonne = 0; colonne < 9; colonne++) {
+                if (this.grille[ligne][colonne]!=0) {
+                    int valeur = this.grille[ligne][colonne];
+                    for (int index=0; index<9; index++) {
+                        if (this.grille[ligne][index]==valeur) if (index!=colonne) return false;
+                        if (this.grille[index][colonne]==valeur) if (index!=ligne) return false;
+                    }
+                    int cLigne = ligne/3;
+                    int cColonne = colonne/3;
+                    for (int index=0+cLigne*3; index<3+cLigne*3; index++) {
+                        for (int jndex=0+cColonne*3; jndex<3+cColonne*3; jndex++) {
+                            if (this.grille[index][jndex]==valeur) if (index!=ligne && jndex!=colonne) return false;
+                        }
+                    }
+                }
             }
-        }
+         }
         return true;
     }
 
     public static void main(String[] args) {
-        int[][] sudokuPartVide = {
-        { 0, 0, 0, 3, 2, 1, 0, 0, 9 },
-        { 0, 8, 0, 6, 7, 4, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 4, 7 },
-        { 4, 0, 6, 0, 0, 7, 0, 0, 2 },
-        { 5, 0, 0, 2, 0, 0, 4, 0, 6 },
-        { 0, 0, 8, 0, 0, 6, 0, 0, 5 },
-        { 0, 4, 0, 0, 0, 2, 1, 0, 8 },
-        { 1, 7, 0, 5, 6, 0, 0, 0, 0 },
-        { 8, 6, 0, 4, 1, 9, 5, 0, 3 }};
-        PremAlgo sudoku = new PremAlgo (sudokuPartVide);
-        long tdebut = new java.util.Date().getTime();
+        int[][] sudokuPartiellementVide = {
+            {0, 0, 5, 4, 1, 2, 0, 0, 0},
+            {0, 0, 0, 0, 5, 7, 1, 3, 4},
+            {0, 0, 0, 8, 0, 0, 0, 5, 0},
+            {0, 1, 0, 0, 4, 0, 0, 9, 0},
+            {5, 9, 0, 0, 7, 0, 0, 0, 8},
+            {4, 0, 8, 0, 6, 5, 0, 0, 0},
+            {0, 5, 0, 7, 0, 4, 8, 6, 0},
+            {0, 7, 0, 0, 0, 0, 5, 0, 0},
+            {8, 6, 0, 5, 9, 1, 0, 7, 0}};
+        
+        long tDebut = new java.util.Date().getTime();
+        PremAlgo sudoku = new PremAlgo (sudokuPartiellementVide);
+        System.out.println("Sudoku partiellement rempli : ");
         sudoku.afficherSudoku();
-        if (sudoku.Resolver()) {
-            System.out.println();
-            System.out.println("L'une des solutions est : ");
-            sudoku.afficherSudoku();
-            System.out.println();
-            System.out.println("L'algo a pris " + (new java.util.Date().getTime() - tdebut) + " milisecondes à resoudre le sudoku");
+        System.out.println();
+        
+        sudoku.resolver();
+        System.out.println();
+        
+        if (sudoku.solution>0) {
+            System.out.println("L'algo a pris " + (sudoku.tSolution - tDebut) + " milisecondes à trouver (et surtout afficher) la premiere solution");
         } else {
             System.out.println ("Il n'y a pas de solution");
         }
