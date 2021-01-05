c4cfb425fec5e5f62a4666b73f664315c16ab165
Ghoneth <74926427+Ghoneth@users.noreply.github.com>
2021-01-05 14:23:09 +0100

    Update Premieralgo.java

--- Premieralgo.java
+++ Premieralgo.java
@@ -5,14 +5,19 @@ public class PremAlgo {
     int iteration=0;
     int solution;
     long tSolution;
-    
-    public PremAlgo (int[][] sudokuPartiellementVide) { //Constructeur contenant le sudoku et son nomre de solution
+   
+/** Constructeur contenant le sudoku, son nombre de solution et le temps de resolution pour la premiere solution.
+ * @param sudokuPartiellementVide La grille 9x9 du sudoku.
+ **/
+    public PremAlgo (int[][] sudokuPartiellementVide) {
         grille=sudokuPartiellementVide;
         solution=0;
         tSolution=0;
     }
     
-    public void afficherSudoku() { //Methode permettant d'afficher un sudoku
+/** Methode simple permettant d'afficher un sudoku.
+ **/
+    public void afficherSudoku() {
         for (int ligne = 0; ligne < 9; ligne++) {
             System.out.print("{");
             for (int colonne = 0; colonne < 9; colonne++) {
@@ -21,9 +26,22 @@ public class PremAlgo {
             }
             System.out.println("}");
         }
-    }    
-
-    public int resolver () { //Méthode récursive résolvant un sudoku en testant chaque case une par une
+    }
+    
+/** Méthode récursive résolvant un sudoku en testant chaque case une par une.
+ * Pour chaque case de la grille 9x9 non vide, la méthode teste chaque nombre de 1 à 9 pour voir si il valide les conditions
+ * d’un sudoku (pas dans ligne, colonne et bloc 3x3), s’il valide les conditions on continue à la case suivante et ainsi
+ * de suite jusqu’à la fin de la grille. Si en revanche on ne trouve pas de nombre validant les conditions alors il
+ * faut revenir en arrière pour tester l’incrémentation de la case d’avant et ainsi de suite. Si finalement on
+ * revient au départ et qu’aucun nombre ne convient, alors il n’y a pas de solution. <p>
+ * 
+ * Lorsque la premiere solution est trouvé, la variable tSolution est définie. <p>
+ * La méthode montre les itérations de recherche pour la premiere solution. <p>
+ * La méthode traite la premiere solution comme une itération fausse et continue à chercher les autres solutions que l'on peut afficher si souhaité.
+ * 
+ * @return La valeur de retour n'est seulement utile que pour la récursivité, on ne s'en sert pas autrement.
+ **/
+    public int resolver () { 
         for (int ligne = 0; ligne < 9; ligne++) {
             for (int colonne = 0; colonne < 9; colonne++) {
                 if (this.grille[ligne][colonne]==0) {
@@ -59,7 +77,11 @@ public class PremAlgo {
         return 0;
     }
 
-    public boolean estValide () { //Méthode testant si un algorithme rempli ou partiellement rempli est correct
+/** Méthode testant si un algorithme rempli ou partiellement rempli est correct.
+ * @return Vrai si le sudoku ne comporte pas deux fois le même nombre dans la même ligne/colonne/bloc 3x3, et
+ * Faux si c'est le cas.
+ **/
+    public boolean estValide () { 
          for (int ligne = 0; ligne < 9; ligne++) {
             for (int colonne = 0; colonne < 9; colonne++) {
                 if (this.grille[ligne][colonne]!=0) {
@@ -80,7 +102,11 @@ public class PremAlgo {
          }
         return true;
     }
-
+    
+/** Main.
+ * Création d'une instance comprenant le sudoku à resoudre, prise de mesure du temps avant la resolution puis
+ * conclusion avec le delta de resolution ou avec le manque de solution.
+ */
     public static void main(String[] args) {
         int[][] sudokuPartiellementVide = {
             {0, 0, 5, 4, 1, 2, 0, 0, 0},
